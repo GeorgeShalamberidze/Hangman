dd55f485c6f018f19c63ea59567f041abc323c01
George Shalamberidze <giorgishalamberidze1995@gmail.com>
2024-07-24 00:25:20 +0400

    rollup

--- src/components/keyboardLetter/index.tsx
+++ src/components/keyboardLetter/index.tsx
@@ -22,7 +22,7 @@ const KeyboardLetter: React.FC<KeyboardLetterPropTypes> = ({
 
 	return (
 		<div
-			className={`p-2 rounded-2xl select-none text-black flex items-center justify-center text-headingS cursor-pointer bg-white ${!selected ? 'opacity-100' : 'opacity-20 pointer-events-none'}`}
+			className={`p-2 rounded-2xl select-none text-black flex items-center justify-center text-headingS cursor-pointer hover:opacity-80 bg-white ${!selected ? 'opacity-100' : 'opacity-20 pointer-events-none'}`}
 			onClick={handleLetterClick}
 		>
 			{value}
